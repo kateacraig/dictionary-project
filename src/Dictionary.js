c8f46eed3daf666415ecd93267491d2f475f44c7
Kate Craig <kate.a.craig@gmail.com>
2025-12-06 16:39:05 -0500

    Added SheCodes Dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`searching for ${keyword} definition`);
+
+    let apiKey = "4a31b94f5oa0a28ee30tf2fd8208d6b6";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
