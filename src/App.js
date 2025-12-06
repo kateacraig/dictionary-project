9197f6249071a436958194104b5e6e1e8f686ef0
Kate Craig <kate.a.craig@gmail.com>
2025-12-06 16:18:04 -0500

    Created Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import Dictionary from "./Dictionary";
 import logo from "./logo.png";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>This was coded by Kate Craig</small>
+        </footer>
+      </div>
     </div>
   );
 }
